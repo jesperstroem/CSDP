6817f7a04c856e92fab427ef49295c6b21e02274
kpl-ece <kpl@ece.au.dk>
2023-10-30 16:24:05 +0100

    Implemented (L)SeqSleepNet factories for data loading and pipelines

--- csdp_pipeline/factories/dataloader_factory.py
+++ csdp_pipeline/factories/dataloader_factory.py
@@ -1,11 +1,15 @@
 from abc import ABC, abstractmethod
 from torch.utils.data import DataLoader
 from csdp_pipeline.pipeline_elements.pipeline_dataset import PipelineDataset
-from csdp_pipeline.factories.pipeline_factory import USleep_Pipeline_Factory
+from csdp_pipeline.factories.pipeline_factory import (
+    USleep_Pipeline_Factory,
+    LSeqSleepNet_Pipeline_Factory,
+)
 from csdp_training.utility import create_split_file
 
+
 class IDataloader_Factory(ABC):
-    def __init__(self, data_split_path, hdf5_base_path):        
+    def __init__(self, data_split_path, hdf5_base_path):
         if data_split_path == None:
             self.data_split_path = create_split_file(hdf5_basepath=hdf5_base_path)
         else:
@@ -18,70 +22,110 @@ class IDataloader_Factory(ABC):
     @abstractmethod
     def create_validation_loader(self, num_workers):
         pass
-    
+
     @abstractmethod
     def create_testing_loader(self, num_workers):
         pass
 
+
 class USleep_Dataloader_Factory(IDataloader_Factory):
-    def __init__(self,
-                 data_split_path,
-                 gradient_steps,
-                 batch_size,
-                 hdf5_base_path,
-                 trainsets,
-                 valsets,
-                 testsets):
+    def __init__(
+        self,
+        data_split_path,
+        gradient_steps,
+        batch_size,
+        hdf5_base_path,
+        trainsets,
+        valsets,
+        testsets,
+    ):
         super().__init__(data_split_path, hdf5_base_path)
 
         self.gradient_steps = gradient_steps
         self.batch_size = batch_size
 
-        self.fac = USleep_Pipeline_Factory(hdf5_base_path,
-                                            self.data_split_path,
-                                            trainsets,
-                                            valsets,
-                                            testsets)
+        self.fac = USleep_Pipeline_Factory(
+            hdf5_base_path, self.data_split_path, trainsets, valsets, testsets
+        )
 
-    def create_training_loader(self, num_workers = 1):
+    def create_training_loader(self, num_workers=1):
         pipes = self.fac.create_training_pipeline()
-        dataset = PipelineDataset(pipes, self.gradient_steps*self.batch_size)
-        trainloader = DataLoader(dataset,
-                                 batch_size=self.batch_size,
-                                 shuffle=False,
-                                 num_workers=num_workers,
-                                 pin_memory=True)
+        dataset = PipelineDataset(pipes, self.gradient_steps * self.batch_size)
+        trainloader = DataLoader(
+            dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            pin_memory=True,
+        )
         return trainloader
-    
-    def create_validation_loader(self, num_workers = 1):
+
+    def create_validation_loader(self, num_workers=1):
         pipes = self.fac.create_validation_pipeline()
         dataset = PipelineDataset(pipes, len(pipes[0].records))
-        valloader = DataLoader(dataset,
-                               batch_size = 1,
-                               shuffle = False,
-                               num_workers = num_workers)
+        valloader = DataLoader(
+            dataset, batch_size=1, shuffle=False, num_workers=num_workers
+        )
         return valloader
 
-    def create_testing_loader(self, num_workers = 1):
+    def create_testing_loader(self, num_workers=1):
         pipes = self.fac.create_test_pipeline()
-        dataset = PipelineDataset(pipes=pipes,
-                                  iterations=len(pipes[0].records))
-        
-        testloader = DataLoader(dataset,
-                                 batch_size=1,
-                                 shuffle=False,
-                                 num_workers=num_workers)
-        return testloader
-    
-class LSeqSleepNet_Dataloader_Factory(IDataloader_Factory):
-    def __init__():
-        pass
+        dataset = PipelineDataset(pipes=pipes, iterations=len(pipes[0].records))
 
-    def create_training_loader(self, num_workers):
-        return super().create_training_loader(num_workers)
-    
-    def create_validation_loader(self, num_workers):
-        return super().create_validation_loader(num_workers)
-    
-    def create_testing_loader(self, num_workers):
-        return super().create_testing_loader(num_workers)
+        testloader = DataLoader(
+            dataset, batch_size=1, shuffle=False, num_workers=num_workers
+        )
+        return testloader
+
+
+class LSeqSleepNet_Dataloader_Factory(IDataloader_Factory):
+    def __init__(
+        self,
+        data_split_path,
+        gradient_steps,
+        batch_size,
+        num_epochs,
+        hdf5_base_path,
+        trainsets,
+        valsets,
+        testsets,
+    ):
+        super().__init__(data_split_path, hdf5_base_path)
+
+        self.gradient_steps = gradient_steps
+        self.batch_size = batch_size
+        self.num_epochs = num_epochs
+
+        self.factory = LSeqSleepNet_Pipeline_Factory(
+            hdf5_base_path=hdf5_base_path,
+            split_path=data_split_path,
+            trainsets=trainsets,
+            valsets=valsets,
+            testsets=testsets,
+            num_epochs=num_epochs,
+        )
+
+    def create_training_loader(self, num_workers=1):
+        pipes = self.factory.create_training_pipeline()
+        dataset = PipelineDataset(pipes, self.gradient_steps * self.batch_size)
+        return DataLoader(
+            dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            pin_memory=True,
+        )
+
+    def create_validation_loader(self, num_workers=1):
+        pipes = self.factory.create_validation_pipeline()
+        dataset = PipelineDataset(pipes, len(pipes[0].records))
+        return DataLoader(
+            dataset, batch_size=self.batch_size, shuffle=False, num_workers=num_workers
+        )
+
+    def create_testing_loader(self, num_workers=1):
+        pipes = self.factory.create_test_pipeline()
+        dataset = PipelineDataset(pipes, len(pipes[0].records))
+        return DataLoader(
+            dataset, batch_size=self.batch_size, shuffle=False, num_workers=num_workers
+        )
--- csdp_pipeline/factories/pipeline_factory.py
+++ csdp_pipeline/factories/pipeline_factory.py
@@ -5,6 +5,7 @@ from csdp_pipeline.pipeline_elements.resampler import Resampler
 from csdp_pipeline.pipeline_elements.spectrogram import Spectrogram
 from csdp_pipeline.pipeline_elements.determ_sampler import Determ_sampler
 
+
 class IPipeline_Factory(ABC):
     @abstractmethod
     def create_training_pipeline(self):
@@ -13,18 +14,14 @@ class IPipeline_Factory(ABC):
     @abstractmethod
     def create_validation_pipeline(self):
         pass
-    
+
     @abstractmethod
     def create_test_pipeline(self):
         pass
 
+
 class USleep_Pipeline_Factory(IPipeline_Factory):
-    def __init__(self,
-                 hdf5_base_path,
-                 split_path,
-                 trainsets,
-                 valsets,
-                 testsets):
+    def __init__(self, hdf5_base_path, split_path, trainsets, valsets, testsets):
         self.split_path = split_path
         self.hdf5_base_path = hdf5_base_path
         self.trainsets = trainsets
@@ -32,41 +29,85 @@ class USleep_Pipeline_Factory(IPipeline_Factory):
         self.testsets = testsets
 
     def create_training_pipeline(self):
-        train_pipes = [Sampler(self.hdf5_base_path,
-                                self.trainsets,
-                                self.split_path,
-                                split_type="train",
-                                num_epochs=35,
-                                subject_percentage = 1.0)]
+        train_pipes = [
+            Sampler(
+                self.hdf5_base_path,
+                self.trainsets,
+                self.split_path,
+                split_type="train",
+                num_epochs=35,
+                subject_percentage=1.0,
+            )
+        ]
         return train_pipes
 
     def create_validation_pipeline(self):
-        val_pipes = [Determ_sampler(self.hdf5_base_path,
-                            self.valsets,
-                            self.split_path,
-                            split_type="val",
-                            num_epochs=35)]
-        
+        val_pipes = [
+            Determ_sampler(
+                self.hdf5_base_path,
+                self.valsets,
+                self.split_path,
+                split_type="val",
+                num_epochs=35,
+            )
+        ]
+
         return val_pipes
 
     def create_test_pipeline(self):
-        test_pipes = [Determ_sampler(self.hdf5_base_path,
-                                     self.testsets,
-                                     self.split_path,
-                                     split_type="test",
-                                     num_epochs=35)]
-        
+        test_pipes = [
+            Determ_sampler(
+                self.hdf5_base_path,
+                self.testsets,
+                self.split_path,
+                split_type="test",
+                num_epochs=35,
+            )
+        ]
+
         return test_pipes
-    
+
+
 class LSeqSleepNet_Pipeline_Factory(IPipeline_Factory):
-    def __init__(self):
-        pass
+    def __init__(
+        self, hdf5_base_path, split_path, trainsets, valsets, testsets, num_epochs
+    ):
+        self.split_path = split_path
+        self.hdf5_base_path = hdf5_base_path
+        self.trainsets = trainsets
+        self.valsets = valsets
+        self.testsets = testsets
+        self.num_epochs = num_epochs
 
     def create_training_pipeline(self):
-        return super().create_training_pipeline()
-    
+        return [
+            Sampler(
+                self.hdf5_base_path,
+                self.trainsets,
+                self.split_path,
+                split_type="train",
+                num_epochs=self.num_epochs,
+                subject_percentage=1.0,
+            ),
+            Resampler(source_sample=128, target_sample=100),
+            Spectrogram(),  # default parameters match SeqSleepNet and derivatives
+        ]
+
+    def __evaluation_pipeline(self, split_type, datasets):
+        return [
+            Determ_sampler(
+                self.hdf5_base_path,
+                datasets,
+                self.split_path,
+                split_type=split_type,
+                num_epochs=self.num_epochs,
+            ),
+            Resampler(source_sample=128, target_sample=100),
+            Spectrogram(),
+        ]
+
     def create_validation_pipeline(self):
-        return super().create_validation_pipeline()
-    
+        return self.__evaluation_pipeline("val", self.valsets)
+
     def create_test_pipeline(self):
-        return super().create_test_pipeline()
+        return self.__evaluation_pipeline("test", self.testsets)
