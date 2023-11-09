a8ff513657dddaa0616cd3183cfba00e30c42f83
jesperstroem <js@ece.au.dk>
2023-11-09 16:03:18 +0100

    .

--- csdp_pipeline/__init__.py
+++ csdp_pipeline/__init__.py
@@ -1,2 +1,2 @@
-from factories.dataloader_factory import USleep_Dataloader_Factory, USleep_Pipeline_Factory
-from preprocessing import spectrogram
+from .factories.dataloader_factory import USleep_Dataloader_Factory, USleep_Pipeline_Factory
+from .preprocessing import spectrogram
